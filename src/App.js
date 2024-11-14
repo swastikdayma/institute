4104183cec85080a3e9ac317ca82a61886fd2165
swastik dayma <daymaswastik17@gmail.com>
2024-11-15 03:06:15 +0530

    added

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
           backgroundPosition: 'center',
           minHeight: '100vh',
           overflowX: 'hidden', // Prevents horizontal scroll
+          overflowY:'hidden'
         }}
       >
         {/* Main App Content */}
